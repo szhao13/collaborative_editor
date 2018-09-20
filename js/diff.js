33fd00ecb85b77ceb2c2b0c9375aae102fe3789c
Shelley Zhao <shelleyzhao@Shelleys-MacBook-Pro.local>
2018-09-20 03:27:19 -0400

    fixed diff and moved files to more standard web project format

--- js/diff.js
+++ js/diff.js
@@ -1,16 +1,18 @@
-//1. Find C table
 /**
- * @param {string} string1
- * @param {string} string2
- * @return {string}
- */
- // m is row dimension, n is column dimension 
- // uses dynamic programming and longest common subsequence alg
+* Returns the difference between two strings using the longest common 
+* subsequence algorithm. 
+*
+* @param stringX
+* @param stringY
+* @return 		 the same, added, and deleted characters 
+*/
+
+
 function diff(stringX, stringY) {
 	var x = stringX.split('');
 	var y = stringY.split('');
 
-	// if (x)
+	// m is row dimension, n is column dimension 
 	var m = x.length;
 	var n = y.length;
 	var lcs = '';
@@ -23,7 +25,6 @@ function diff(stringX, stringY) {
 	var lcsColumnIndex = 0;
 
 	// calculate LCS table via dynamic programming
-	
 	for (var i = 0; i <= m; i++) {
 		c[i] = [];
 		for (var j = 0; j <= n; j++) {
@@ -40,34 +41,29 @@ function diff(stringX, stringY) {
 
 			// Look for longest LCS, update length, column and row indices if found
 			// to use for backtrace
-			if (c[i][j] > lcsLength) {
-				// console.log("c[i][j]: " + c[i][j])
-				lcsLength = c[i][j];
-				lcsRowIndex = i;
-				lcsColumnIndex = j;
-			}
+			// if (c[i][j] > lcsLength) {
+			// 	// console.log("c[i][j]: " + c[i][j])
+			// 	lcsLength = c[i][j];
+			// 	lcsRowIndex = i;
+			// 	lcsColumnIndex = j;
+			// }
 		}
 
 	}
 
-	// console.log(c)
 
 	var lcs = backtrace(c, x, y, m, n);
-
 	// console.log(c);
-
-	// console.log(lcs);
-	// console.log(lcsColumnIndex)
-	// console.log(lcsLength)
-	var same = [];
-	var added = [];
-	var deleted = [];
+	// var same = [];
+	// var added = [];
+	// var deleted = [];
+	var result = [];
 	// console.log(diff(c, x, y, m, n, same, added, deleted));
-	var diffResult = findDiff(c, x, y, m, n, same, added, deleted);
+	var diffResult = findDiff(c, x, y, m, n, result);
 	return diffResult;
 }
 
-// recursive function to backtrace through lcs array
+// Recursively backtrace through lcs array
 function backtrace(c, x, y, i, j) {
 	if (i === 0 || j === 0) {
 		return ''
@@ -81,25 +77,25 @@ function backtrace(c, x, y, i, j) {
 	return backtrace(c, x, y, i-1, j);
 }
 
-
-function findDiff(c, x, y, i, j, same, added, deleted) {
+// Recursively find same, added and deleted characters
+function findDiff(c, x, y, i, j, result) {
 	if (i > 0 && j > 0 && x[i-1] === y[j-1]) {
-		findDiff(c, x, y, i-1, j-1, same, added, deleted);
-		same.push(x[i-1]);
-		// console.log(" " + x[i-1]);
+		findDiff(c, x, y, i-1, j-1, result);
+		
+		result.push(" " + x[i-1]);
 	}
 	else if (j > 0 && (i == 0 || c[i][j-1] >= c[i-1][j])) {
-		findDiff(c, x, y, i, j-1, same, added, deleted);
-		added.push(y[j]);
-		// console.log("+ " + y[j]);
+		findDiff(c, x, y, i, j-1, result);
+		// added.push(y[j-1]);
+		result.push("+" + y[j-1]);
 	}
 	else if (i > 0 && (j == 0 || c[i][j-1] < c[i-1][j])) {
-		findDiff(c, x, y, i-1, j, same, added, deleted);
-		deleted.push(x[i-1]);
-		// console.log("- " + x[i-1]);
+		findDiff(c, x, y, i-1, j, result);
+		// deleted.push(x[i-1]);
+		result.push("-" + x[i-1]);
 	}
 
-	return {same: same, added: added, deleted: deleted};
+	return result;
 	// else {
 	// 	console.log(" ");
 	// }
