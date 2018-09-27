e473034a89eb09e7befc919845fb990b2417de57
Shelley Zhao <shelleyzhao@Shelleys-MacBook-Pro.local>
2018-09-27 02:41:47 -0400

    fixed diff outputs, diffs are still not combined

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var ot = require('ot');
 
+// Note: uses ot.js to resolve differences between two users
 $(document).ready(function() {
   // var ot = require('ot');
   var old_input = "";
@@ -11,67 +12,61 @@ $(document).ready(function() {
     var str_2 = old_input;
     var new_input_1 = $("#textarea_1").val();
     var new_input_2 = $("#textarea_2").val();
-    console.log("old input is " + old_input);
     // get difference between old and new input for user 1 and user 2
-    var diff_result_1 = diff(old_input, new_input_1);
-    var diff_result_2 = diff(old_input, new_input_2);
+    var diff_results_1 = diff(old_input, new_input_1, 1);
+    var diff_results_2 = diff(old_input, new_input_2, 2);
 
-    // console.log("diff result 1 is " + diff_result_1);
-    var operation_combo_1 = diffToOT(diff_result_1, str_1);
+
+    // Combine operations for user 1 
+    var operation_combo_1 = diffToOT(diff_results_1, str_1);
     var s1 = operation_combo_1.apply(str_1);
-    // console.log("operation for user 1 is " + operation_combo_1);
-    // var operation_combo_2 = new ot.TextOperation()
-    //   .retain(0);
-    var operation_combo_2 = diffToOT(diff_result_2, str_2);
+
+    // Combine operations for user 2
+    var operation_combo_2 = diffToOT(diff_results_2, str_2);
     var s2 = operation_combo_2.apply(str_2);
-
-    // console.log("operation for user 2 is " + operation_combo_2);
-
 
     var s1 = operation_combo_1.apply(old_input);
     var s2 = operation_combo_2.apply(old_input);
-    var user_1_edits = diff(str_1, s1);
-    var user_2_edits = diff(str_2, s2);
 
+    // Find edits made by user 1 and user 2 
+    var user_1_edits = diff(str_1, s1, 1);
+    var user_2_edits = diff(str_2, s2, 2);
+
+    // Use operational transform to resolve differences between
+    // two users
     var transformedPair = ot.TextOperation.transform(operation_combo_1, operation_combo_2);
     var operation1Prime = transformedPair[0];
     var operation2Prime = transformedPair[1];
-    // console.log("1 Prime is " + operation1Prime);
-    // console.log("2 Prime is " + operation2Prime); 
-    // console.log("operation2prime's base length is " + operation2Prime.baseLength);
-    // console.log("old_input's length is " + old_input.length);
-    var str12Prime = operation1Prime.apply(s2); // "ipsum dolor"
+
+    var str12Prime = operation1Prime.apply(s2); 
     var str21Prime = operation2Prime.apply(s1);
     
-    var user_2_edits_on_1 = diff(s1, str21Prime);
-
-    console.log(user_2_edits_on_1);
-
-    console.log("str12Prime is " + str12Prime);
-    console.log("str21Prime is " + str21Prime);
-
-    
+    // get edits from user 2 
+    var user_2_edits_on_1 = diff(s1, str21Prime, 2);
 
 
-    // empty previous output
+    // replace previous output with current
+    // Note - the diffs for each user are displayed
+    // on two different lines, due to a bug
     $("#textarea_1").val(str12Prime);
-    $("#textarea_2").val(str12Prime); 
-
-
-    // apply changes found by diff to operation
+    $("#textarea_2").val(str21Prime); 
 
     // build new output by adding each char as a new span element of
     // the appropriate class (added, same, deleted)
-    $("#output").empty();
-    displayDiff(user_1_edits, 2);
+    $("#output1").empty();
+    $("#output2").empty();
+
+    displayDiff(user_1_edits, 1);
+    displayDiff(user_2_edits_on_1, 2);
+
+    // Replace old input with the current output
     old_input = str12Prime;
 
-    console.log("old_input at end of update is " + old_input);
   });
 });
 
-// Converts results of diff to a TextOperation object for OT 
-function diffToOT(diff_result, str) {
+// Converts results of diff to a an operation for OTs
+function diffToOT(diff_results, str) {
   var len = str.length;
 
   // the index of the output string
@@ -85,63 +80,93 @@ function diffToOT(diff_result, str) {
   
   // iterate through diff array, add an operation to the combined
   // operation for each added and deleted character 
-    $.each(diff_result, function(diff_i, value) {
-      
-      if (value[0] === "+") {
+    // $.each(diff_results, function(diff_i, diff) {
+    for (var i = 0; i < diff_results.length; i++) {
+
+    var status = diff_results[i].status;
+    var char = diff_results[i].char;
+    var user = diff_results[i].user;
+      if (status === "+") {
         var operation_insert = new ot.TextOperation()
           .retain(output_i)
-          .insert(value[1])
+          .insert(char)
           .retain(len - output_i);
-
         operation_combo = operation_combo.compose(operation_insert);
         len++;
         output_i++;
       }
-      else if (value[0] === " ") {
+      else if (status === " ") {
         output_i++;
       }
-      else if (value[0] === '-') {
+      else if (status === '-') {
         var operation_delete = new ot.TextOperation()
           .retain(output_i)
-          .delete(value[1])
+          .delete(char)
           .retain(len - output_i - 1);
         var retain_val = len - output_i - 1;
-        console.log("operation delete is " + operation_delete);
-        console.log("number of chars remaining in str after delete is " + retain_val);
-        console.log("combined operation before compose is " + operation_combo);
-
+  
         operation_combo = operation_combo.compose(operation_delete);
 
         len--;
   
-      }
-    });
+      };
+    }
     return operation_combo;
 };
 
-
-function displayDiff(diff_result, user) {
-  console.log("we get to displayDiff");
-  $.each(diff_result, function(index, value) {
+// create elements to display diff logs
+// Note: diff logs for user 1 and user 2 are on separate lines due to a 
+// bug. 
+function displayDiff(diff_results, user) {
+  for (var i = 0; i < diff_results.length; i++) {
+    var status = diff_results[i].status;
+    var char = diff_results[i].char;
+    var user = user;
     var add_char = "";
-    if (value[0] === "+") {
-      add_char = "<span class=\"added" + user + "\">" + value[1] + "</span>";
-      console.log("added element is " + add_char);
-      $("#output").append(add_char);
+    if (status === "+") {
+      add_char = "<span class=\"added" + user + "\">" + char + "</span>";
+      if (user === 1) {
+        $("#output1").append(add_char);
+      }
+      else {
+        $("#output2").append(add_char);
+
+      }
     }
-    else if (value[0] === " ") {
-      add_char = "<span class=\"same\">" + value[1] + "</span>";
-      $("#output").append(add_char);
+    else if (status === " ") {
+      add_char = "<span class=\"same\">" + char + "</span>";
+      if (user === 1) {
+        $("#output1").append(add_char);
+      }
+      else {
+        $("#output2").append(add_char);
+
+      }
     }
-    else if (value[0] === '-') {
-      add_char =  "<span class=\"deleted" + user + "\">" + value[1] + "</span>";
-      $("#output").append(add_char);
+    else if (status === '-') {
+      add_char =  "<span class=\"deleted" + user + "\">" + char + "</span>";
+      if (user === 1) {
+        $("#output1").append(add_char);
+      }
+      else {
+        $("#output2").append(add_char);
+      }
     }
-  });
+  };
 
 
 };
 
+
+// defines an object that contains each char of the diff'd string
+// status is either "+", "-", " " for added, deleted or no change respectively
+// User is either 0, 1 or 2, for unchanged, edited by user 1
+// or edited by user 2, respectively
+function DiffResult(status, char, user) {
+  this.status = status;
+  this.char = char;
+  this.user = user;
+}
 /**
 * Returns the difference between two strings using the longest common 
 * subsequence algorithm. 
@@ -160,8 +185,7 @@ function displayDiff(diff_result, user) {
 * // Prints ["-X", " M", "+Z", " J", "-Y", " A", "+W", "+X", " U", "-Z"]    
 **/
 
-// outputs an array of strings, where the first char in the string is eit
-function diff(stringX, stringY) {
+function diff(stringX, stringY, user) {
   var x = stringX.split('');
   var y = stringY.split('');
 
@@ -186,7 +210,6 @@ function diff(stringX, stringY) {
       else {
         c[i][j] = Math.max(c[i-1][j], c[i][j-1]);
       }
-      // console.log(c);
 
     }
 
@@ -194,14 +217,14 @@ function diff(stringX, stringY) {
 
   lcs = backtrace(c, x, y, m, n);
 
-  var result = [];
-  var diff_result = findDiff(c, x, y, m, n, result);
+  var results = [];
+  var diff_results = findDiff(c, x, y, m, n, results, user);
 
   // for (var i = 0; i < diff_result.length; i++) {
   //   if (diff_result[i][])
   // }
-  console.log(diff_result);
-  return diff_result;
+  // console.log(diff_results);
+  return diff_results;
 }
 
 // Recursively backtrace through lcs array
@@ -219,20 +242,33 @@ function backtrace(c, x, y, i, j) {
 }
 
 // Recursively find same, added and deleted characters
-function findDiff(c, x, y, i, j, result) {
+function findDiff(c, x, y, i, j, results, user) {
   if (i > 0 && j > 0 && x[i-1] === y[j-1]) {
-    findDiff(c, x, y, i-1, j-1, result);
-    result.push(" " + x[i-1]);
+    findDiff(c, x, y, i-1, j-1, results, user);
+    diff_result = new DiffResult();
+    diff_result.char = x[i-1];
+    diff_result.status = " ";
+    diff_result.user = 0;
+    results.push(diff_result);
   }
   else if (j > 0 && (i == 0 || c[i][j-1] >= c[i-1][j])) {
-    findDiff(c, x, y, i, j-1, result);
-    result.push("+" + y[j-1]);
+    findDiff(c, x, y, i, j-1, results, user);
+    diff_result = new DiffResult();
+    diff_result.char = y[j-1];
+    diff_result.status = "+";
+    diff_result.user = user;
+    results.push(diff_result);
+
   }
   else if (i > 0 && (j == 0 || c[i][j-1] < c[i-1][j])) {
-    findDiff(c, x, y, i-1, j, result);
-    result.push("-" + x[i-1]);
+    findDiff(c, x, y, i-1, j, results, user);
+    diff_result = new DiffResult();
+    diff_result.char = x[i-1];
+    diff_result.status = "-";
+    diff_result.user = user;
+    results.push(diff_result);   
   }
 
-  return result;
+  return results;
 
 }
